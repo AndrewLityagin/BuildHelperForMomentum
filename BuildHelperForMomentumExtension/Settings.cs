6ae4417eefce3a752514ace1406e6d5d5e4928d3
Andrew Lityagin <aalityagin@bk.ru>
2022-11-04 22:45:06 +0400

    Add projects files

---
+++ BuildHelperForMomentumExtension/Settings.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BuildHelperForMomentumExtension
+{
+	public class Settings : INotifyPropertyChanged
+	{
+		
+
+		private bool replaceWebProjects;
+
+		public bool ReplaceWebProjects
+		{
+			get { return replaceWebProjects; }
+			set
+			{
+				if(value != replaceWebProjects)
+				{
+					replaceWebProjects = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(ReplaceWebProjects));
+				}
+			}
+		}
+
+		private bool killProcess;
+		public bool KillProcess
+		{
+			get { return killProcess; }
+			set
+			{
+				if(value != killProcess)
+				{
+					killProcess = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(KillProcess));
+				}
+			}
+		}
+
+		private bool autoChoosingRepos;
+		public bool AutoChoosingRepos
+		{
+			get { return autoChoosingRepos; }
+			set
+			{
+				if(value != autoChoosingRepos)
+				{
+					autoChoosingRepos = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(AutoChoosingRepos));
+				}
+			}
+		}
+
+		private byte timePeriod;
+		public byte TimePeriod
+		{
+			get { return timePeriod; }
+			set
+			{
+				if(value != timePeriod)
+				{
+					timePeriod = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(TimePeriod));
+				}
+			}
+		}
+
+		private string reposFolder;
+		public string ReposFolder
+		{
+			get { return reposFolder; }
+			set
+			{	
+				if(value != reposFolder)
+				{
+					if(autoChoosingRepos)
+						reposFolder = value;
+					else
+						reposFolder = Directory.GetCurrentDirectory();
+					webProjectSupervisorFolderFromRepos = $"{reposFolder}\\Binaries\\Web\\Momentum.Web.Supervisor\\net6.0\\publish\\wwwroot";
+					webProjectOperatorFolderFromRepos = $"{reposFolder}\\Binaries\\Web\\Momentum.Web.Operator\\net6.0\\publish\\wwwroot";
+					SaveToFile();
+					OnPropertyChanged(nameof(ReposFolder));
+					OnPropertyChanged(nameof(WebProjectSupervisorFolderFromRepos));
+					OnPropertyChanged(nameof(WebProjectSupervisorFolderFromRepos));
+				}
+			}
+		}
+
+		private string instanceFolder;
+		public string InstanceFolder
+		{
+			get { return instanceFolder; }
+			set
+			{
+				if(value != instanceFolder)
+				{
+					instanceFolder = value;
+					webProjectSupervisorFolderFromInstance = $"{instanceFolder}\\WebServer\\Default\\Momentum.Supervisor\\wwwroot";
+					webProjectOperatorFolderFromInstance = $"{instanceFolder}\\WebServer\\Default\\Momentum.Operator\\wwwroot";
+					SaveToFile();
+					OnPropertyChanged(nameof(InstanceFolder));
+					OnPropertyChanged(nameof(WebProjectOperatorFolderFromInstance));
+					OnPropertyChanged(nameof(WebProjectOperatorFolderFromInstance));
+				}
+			}
+		}
+
+		private string webProjectSupervisorFolderFromRepos;
+		public string WebProjectSupervisorFolderFromRepos
+		{
+			get { return webProjectSupervisorFolderFromRepos; }
+			set
+			{
+				if(value != webProjectSupervisorFolderFromRepos)
+				{
+					webProjectSupervisorFolderFromRepos = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(WebProjectSupervisorFolderFromRepos));
+				}
+			}
+		}
+
+		private string webProjectSupervisorFolderFromInstance;
+		public string WebProjectSupervisorFolderFromInstance
+		{
+			get { return webProjectSupervisorFolderFromInstance; }
+			set
+			{
+				if(value != webProjectSupervisorFolderFromInstance)
+				{
+					webProjectSupervisorFolderFromInstance = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(WebProjectSupervisorFolderFromInstance));
+				}
+			}
+		}
+
+		private string webProjectOperatorFolderFromRepos;
+		public string WebProjectOperatorFolderFromRepos
+		{
+			get { return webProjectOperatorFolderFromRepos; }
+			set
+			{
+				if(value != webProjectOperatorFolderFromRepos)
+				{
+					webProjectOperatorFolderFromRepos = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(WebProjectOperatorFolderFromRepos));
+				}
+			}
+		}
+
+		private string webProjectOperatorFolderFromInstance;
+		public string WebProjectOperatorFolderFromInstance
+		{
+			get { return webProjectOperatorFolderFromInstance; }
+			set
+			{
+				if(value != webProjectOperatorFolderFromInstance)
+				{
+					webProjectOperatorFolderFromInstance = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(WebProjectOperatorFolderFromInstance));
+				}
+			}
+		}
+
+
+		private string customFolderToRemoveFromInstance;
+		public string CustomFolderToRemoveFromInstance
+		{
+			get { return customFolderToRemoveFromInstance; }
+			set
+			{
+				if(value != customFolderToRemoveFromInstance)
+				{
+					customFolderToRemoveFromInstance = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(CustomFolderToRemoveFromInstance));
+				}
+			}
+		}
+
+
+		private string customFolderToRemoveFromRepos;
+		public string CustomFolderToRemoveFromRepos
+		{
+			get { return customFolderToRemoveFromRepos; }
+			set
+			{
+				if(value != customFolderToRemoveFromRepos)
+				{
+					customFolderToRemoveFromRepos = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(CustomFolderToRemoveFromRepos));
+				}
+			}
+		}
+
+		private string programDirectory;
+		public string ProgramDirectory { 
+			get { 
+				return programDirectory; 
+			} 
+			set
+			{
+				if(value != programDirectory)
+				{
+					programDirectory = value;
+					SaveToFile();
+					OnPropertyChanged(nameof(ProgramDirectory));
+				}
+			}
+		}
+
+		private static Settings instance = null;
+		private static string confDirectory = $"C:\\Users\\{Environment.UserName}\\.buildHelperExtension";
+		private static string confFileName = "build_helper_extension.json";
+		private static string confFile = $"{confDirectory}\\{confFileName}";
+										   
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName] string prop = "")
+		{
+			if(PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+		}
+		public static Settings GetInstance()
+		{
+			if(instance == null)
+			{
+				if(!File.Exists(confFile))
+				{
+					if(!Directory.Exists(confDirectory))
+						Directory.CreateDirectory(confDirectory);
+					using(FileStream fs = new FileStream(confFile, FileMode.OpenOrCreate))
+					{
+						var userDirectory = $"C:\\Users\\{Environment.UserName}";
+						instance = new Settings(5,
+							 false,
+							 true,
+							 true,
+							 $"{userDirectory}\\source\\repos\\MEScontrol.net Standard",
+							 $"C:\\Program Files (x86)\\BrightEye\\Momentum\\Happy Sauce 17.3",
+							 $"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86)}\\BuildHelperForMomentumExtension",
+							 $"{userDirectory}\\source\\repos\\MEScontrol.net Standard\\Binaries\\Web\\Momentum.Web.Supervisor\\net6.0\\publish\\wwwroot",
+							 $"{userDirectory}\\source\\repos\\MEScontrol.net Standard\\Binaries\\Web\\Momentum.Web.Operator\\net6.0\\publish\\wwwroot",
+							 $"C:\\Program Files (x86)\\BrightEye\\Momentum\\Happy Sauce 17.3\\WebServer\\Default\\Momentum.Supervisor\\wwwroot",
+							 $"C:\\Program Files (x86)\\BrightEye\\Momentum\\Happy Sauce 17.3\\WebServer\\Default\\Momentum.Operator\\wwwroot",
+							 "",
+							 "");
+
+						JsonSerializer.Serialize<Settings>(fs, instance);
+					}
+				}
+				else
+				{
+					using(FileStream fs = new FileStream(confFile, FileMode.Open))
+					{
+						try
+						{
+							instance = JsonSerializer.Deserialize<Settings>(fs);
+
+						}
+						catch(Exception ex)
+						{
+							Console.WriteLine(ex.Message);
+						}
+					}
+				}
+			}
+			return instance;
+		}
+
+		public static void SaveToFile()
+		{
+			using(FileStream fs = new FileStream(confFile, FileMode.Create))
+			{
+				JsonSerializer.Serialize<Settings>(fs, instance);
+			}
+		}
+
+		public Settings(byte timePeriod, bool killProcess, bool replaceWebProjects, bool autoChoosingRepos, 
+			string reposFolder, string instanceFolder,string programDirectory,
+			string webProjectSupervisorFolderFromRepos, string webProjectOperatorFolderFromRepos, 
+			string webProjectSupervisorFolderFromInstance, string webProjectOperatorFolderFromInstance, 
+			string customFolderToRemoveFromRepos,string customFolderToRemoveFromInstance)
+		{
+			this.timePeriod = timePeriod;
+			this.killProcess = killProcess;
+			this.replaceWebProjects = replaceWebProjects;
+			this.autoChoosingRepos = autoChoosingRepos;
+			this.instanceFolder = instanceFolder;
+			this.reposFolder = reposFolder;
+			this.instanceFolder = instanceFolder;
+			this.programDirectory = programDirectory;
+			this.webProjectSupervisorFolderFromRepos = webProjectSupervisorFolderFromRepos;
+			this.webProjectOperatorFolderFromRepos = webProjectOperatorFolderFromRepos;
+			this.webProjectSupervisorFolderFromInstance = webProjectSupervisorFolderFromInstance;
+			this.webProjectOperatorFolderFromInstance = webProjectOperatorFolderFromInstance;
+			this.customFolderToRemoveFromInstance = customFolderToRemoveFromInstance;
+			this.customFolderToRemoveFromRepos = customFolderToRemoveFromRepos;
+		}
+	}
+}
---
+++ FileReplacer/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using BuildHelperForMomentumExtension;
+
+namespace FileReplacer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MoveCompiledFilesProc();
+		}
+		private static void KillProcess()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkCyan;
+			Console.WriteLine("  0. Killing process");
+			var process = new string[]{"MEScontrol.DMS.Server","MEScontrol.MES.Server","MEScontrol.LMS.LabelServer",
+								"MEScontrol.DataCenter.Server","MEScontrol.WMS.Server","MEScontrol.Shell",
+								"Momentum.WebServer","Momentum.Manager","Momentum.PrintServer",
+								"Momentum.Server","MEScontrol.Configuration"};
+			foreach(var prc in process)
+			{
+				try
+				{
+					foreach(Process proc in Process.GetProcessesByName(prc))
+					{
+						proc.Kill();
+					}
+				}
+				catch(Exception ex)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine($"  Error in killing process {ex.Message}");
+				}
+			}
+		}
+
+		private static string[] GetUpdatedFiles(string folder, byte timePeriod)
+		{
+			var binaryFiles = new List<string>();
+
+			var exeFiles = Directory.GetFiles(folder, "*.exe", SearchOption.AllDirectories);
+			var dllFiles = Directory.GetFiles(folder, "*.dll", SearchOption.AllDirectories);
+			var pdbFiles = Directory.GetFiles(folder, "*.pdb", SearchOption.AllDirectories);
+
+			if(dllFiles.Length != 0 && pdbFiles.Length != 0)
+			{
+				binaryFiles.AddRange(dllFiles);
+				binaryFiles.AddRange(pdbFiles);
+			}
+			if(exeFiles.Length != 0)
+				binaryFiles.AddRange(exeFiles);
+
+			int i = 0;
+			List<string> updatedFiles = new List<string>();
+			var now = DateTime.Now;
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.WriteLine("  Looking for updated files in repository folder.");
+
+			foreach(var file in binaryFiles)
+			{
+				var fileUpdate = File.GetLastWriteTime(file);
+				if(Math.Abs(now.Subtract(fileUpdate).TotalMinutes) <= timePeriod)
+				{
+					updatedFiles.Add(file);
+					i++;
+				}
+			}
+			Console.WriteLine($"  Found {i} files\n\n");
+			return updatedFiles.ToArray();
+		}
+
+		private static void MoveCompiledFilesProc()
+		{
+			try
+			{
+				var settings = Settings.GetInstance();
+				Console.ForegroundColor = ConsoleColor.DarkBlue;
+				Console.WriteLine("Build Helper For Momentum extension");
+				Console.WriteLine("  Settings:");
+				Console.WriteLine($"  {nameof(settings.KillProcess)} : {settings.KillProcess}");
+				Console.WriteLine($"  {nameof(settings.InstanceFolder)} : {settings.InstanceFolder}");
+				Console.WriteLine($"  {nameof(settings.ReposFolder)} : {settings.ReposFolder}");
+				Console.WriteLine($"  {nameof(settings.TimePeriod)} : {settings.TimePeriod}");
+				Console.WriteLine($"  {nameof(settings.WebProjectOperatorFolderFromInstance)} : {settings.WebProjectOperatorFolderFromInstance}");
+				Console.WriteLine($"  {nameof(settings.WebProjectOperatorFolderFromRepos)} : {settings.WebProjectOperatorFolderFromRepos}");
+				Console.WriteLine($"  {nameof(settings.WebProjectSupervisorFolderFromInstance)} : {settings.WebProjectSupervisorFolderFromInstance}");
+				Console.WriteLine($"  {nameof(settings.WebProjectSupervisorFolderFromRepos)} : {settings.WebProjectSupervisorFolderFromRepos}");
+
+				if(settings.KillProcess)
+					KillProcess();
+
+				var updatedFiles = GetUpdatedFiles($"{settings.ReposFolder}\\Binaries", settings.TimePeriod).Where(file => !file.Contains("\\Binaries\\Web\\"));
+
+				Console.ForegroundColor = ConsoleColor.DarkGreen;
+				Console.WriteLine("  Replacing updated files to instance folder...");
+				int i = 0;
+				foreach(var uFile in updatedFiles)
+				{
+					var fileName = uFile.Remove(0, uFile.LastIndexOf("\\") + 1);
+					var previousFolder = string.Empty;
+					if(fileName.EndsWith(".resources.dll") || uFile.Contains("NiceLabel"))
+					{
+						var temp = uFile.Remove(uFile.LastIndexOf("\\"), uFile.Length - uFile.LastIndexOf("\\"));
+						previousFolder = uFile.Remove(0, temp.LastIndexOf("\\"));
+						previousFolder = previousFolder.Remove(previousFolder.LastIndexOf(fileName), fileName.Length);
+					}
+
+					var filesToRepalce = Directory.GetFiles(settings.InstanceFolder, $"{fileName}", SearchOption.AllDirectories).Where(file => !file.Contains("WebServer"));
+
+					foreach(var rFile in filesToRepalce)
+					{
+						var sizeDifference = (new FileInfo(uFile)).Length / (new FileInfo(rFile)).Length;
+						if(previousFolder == string.Empty && (sizeDifference <= 1.3) || (previousFolder != string.Empty && rFile.Contains(previousFolder)))
+						{
+							Console.WriteLine($"  #{i} {uFile} =>\n  {rFile}");
+							File.Delete(rFile);
+							File.Copy(uFile, rFile);
+							i++;
+						}
+					}
+				}
+				Console.WriteLine($"  Replaced {i} files.\n\n");
+
+				if(settings.ReplaceWebProjects && Directory.Exists($"{settings.ReposFolder}\\Binaries\\Web"))
+				{
+					Console.WriteLine("  Replacing updated files to instance folder (web applications)...");
+					updatedFiles = GetUpdatedFiles($"{settings.ReposFolder}\\Binaries\\Web", settings.TimePeriod);
+					i = 0;
+					foreach(var uFile in updatedFiles)
+					{
+						var fileName = uFile.Remove(0, uFile.LastIndexOf("\\") + 1);
+						var previousFolder = string.Empty;
+						if(fileName.EndsWith(".resources.dll") || uFile.Contains("NiceLabel"))
+						{
+							var temp = uFile.Remove(uFile.LastIndexOf("\\"), uFile.Length - uFile.LastIndexOf("\\"));
+							previousFolder = uFile.Remove(0, temp.LastIndexOf("\\"));
+							previousFolder = previousFolder.Remove(previousFolder.LastIndexOf(fileName), fileName.Length);
+						}
+
+						var filesToRepalce = Directory.GetFiles(settings.InstanceFolder, $"{fileName}", SearchOption.AllDirectories).Where(file => file.Contains("\\WebServer\\"));
+
+						foreach(var rFile in filesToRepalce)
+						{
+							var sizeDifference = (new FileInfo(uFile)).Length / (new FileInfo(rFile)).Length;
+							if(previousFolder == string.Empty && (sizeDifference <= 1.3) || (previousFolder != string.Empty && rFile.Contains(previousFolder)))
+							{
+								Console.WriteLine($"  #{i} {uFile} =>\n  {rFile}");
+								File.Delete(rFile);
+								File.Copy(uFile, rFile);
+								i++;
+							}
+						}
+					}
+					Console.WriteLine($"  Replaced {i} files.\n\n");
+				}
+
+				Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+				if(settings.ReplaceWebProjects && Directory.Exists(settings.WebProjectOperatorFolderFromInstance) && Directory.Exists(settings.WebProjectOperatorFolderFromRepos))
+				{
+					Console.WriteLine("Replacing Operator Web Project folder.");
+					Directory.Delete(settings.WebProjectOperatorFolderFromInstance, true);
+					Directory.Move(settings.WebProjectOperatorFolderFromRepos, settings.WebProjectOperatorFolderFromInstance);
+
+				}
+
+				if(settings.ReplaceWebProjects && Directory.Exists(settings.WebProjectSupervisorFolderFromInstance) && Directory.Exists(settings.WebProjectSupervisorFolderFromRepos))
+				{
+					Console.WriteLine("Replacing Supervisor Web Project folder.");
+					Directory.Delete(settings.WebProjectSupervisorFolderFromInstance, true);
+					Directory.Move(settings.WebProjectSupervisorFolderFromRepos, settings.WebProjectSupervisorFolderFromInstance);
+				}
+
+				if(Directory.Exists(settings.CustomFolderToRemoveFromInstance) && Directory.Exists(settings.CustomFolderToRemoveFromRepos))
+				{
+					Console.WriteLine("Replacing custom folder...");
+					Directory.Delete(settings.CustomFolderToRemoveFromInstance, true);
+					Directory.Move(settings.CustomFolderToRemoveFromRepos, settings.CustomFolderToRemoveFromInstance);
+				}
+
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine("All operations finished.");
+			}
+			catch(Exception ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine($"Error: {ex.Message}");
+				Console.WriteLine("Replacement unsuccessful");
+			}
+			Console.ReadKey();
+		}
+	}
+}
